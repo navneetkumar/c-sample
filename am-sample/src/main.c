e04d1cf9301d7da4ed4deb8ad7ace383abb1c648
Navneet Kumar <navneetk@bluejeans.com>
2017-01-22 19:03:12 +0530

    changeing

--- am-sample/src/main.c
+++ am-sample/src/main.c
@@ -1,10 +1,13 @@
 #include <config.h>
 #include <stdio.h>
+#include <heymath.h>
 
 int
 main (void)
 {
   puts ("Hello World!");
   puts ("This is " PACKAGE_STRING ".");
+  int x= 10, y = 20;
+  printf("\n%d + %d = %d", x, y, add(x, y));
   return 0;
 }
